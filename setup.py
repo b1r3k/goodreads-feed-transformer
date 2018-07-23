1345b89d7b416ad2afe46556b652dd4a6f7141a8
Lukasz Jachym <lukasz.jachym@gmail.com>
2018-07-23 13:45:32 +0200

    fixes: requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from setuptools import setup
 
 version = '0.0.1'
 
+with open('requirements.txt', 'r') as f:
+    install_reqs = [
+        s for s in [
+            line.strip(' \n') for line in f
+        ] if not s.startswith('#') and s != ''
+    ]
+
 setup(name='feed-transformer',
       version=version,
       description='goodreads-feed-transformer',
@@ -13,6 +20,7 @@ setup(name='feed-transformer',
           'Programming Language :: Python :: 3.6',
           'Operating System :: POSIX',
       ],
+      install_reqs=install_reqs,
       entry_points={
           'paste.app_factory': ['app = transformer:app'],
       })
