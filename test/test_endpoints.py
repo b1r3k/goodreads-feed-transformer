c6be752fe4664f303b810f0617b7de5854a5738d
Lukasz Jachym <lukasz.jachym@gmail.com>
2019-09-10 17:17:07 +0200

    adds: github webhooks handler

--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+from io import BytesIO
+from http.server import BaseHTTPRequestHandler
+
 from asynctest import CoroutineMock, MagicMock
 from aiohttp import web
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
@@ -6,13 +10,39 @@ import transformer
 from transformer import const
 
 
+def read_fixture(path, decoder=None):
+    fixture_path = Path('test', 'fixtures', path)
+
+    with open(fixture_path.resolve(), 'rb') as fd:
+        content = fd.read()
+        if decoder:
+            return decoder(content)
+        else:
+            return str(content, 'utf-8')
+
+
+class HTTPRequest(BaseHTTPRequestHandler):
+    def __init__(self, request_text):
+        self.rfile = BytesIO(request_text)
+        self.raw_requestline = self.rfile.readline()
+        self.error_code = self.error_message = None
+        self.parse_request()
+        # BaseHTTPRequestHandler.parse_request encodes string with iso-8859-1
+        self.path = str(bytes(self.path, 'iso-8859-1'), 'utf-8')
+
+    def send_error(self, code, message=None, explain=None):
+        raise ValueError("code %d, message %s" % (code, message))
+
+
 class HttpRouting(AioHTTPTestCase):
     async def get_application(self):
         app = web.Application()
         app[const.DB_REDIS] = redis_mock = MagicMock()
         redis_mock.setnx = CoroutineMock()
         redis_mock.setex = CoroutineMock()
-        app[const.SETTINGS] = {}
+        app[const.SETTINGS] = {
+            const.GH_SECRET: 'GH_SECRET'
+        }
         app[const.HTTP_SESSION] = CoroutineMock()
         transformer.main.initialize_endpoints(app)
         return app
@@ -29,3 +59,11 @@ class HttpRouting(AioHTTPTestCase):
         assert resp.status == 200
         self.app[const.DB_REDIS].setnx.assert_awaited()
         self.app[const.DB_REDIS].setex.assert_awaited()
+
+    @unittest_run_loop
+    async def test_github_webhook(self):
+        req = read_fixture('github_ping.req', decoder=HTTPRequest)
+        body = req.rfile.read()
+        resp = await self.client.request("POST", "/github/b1r3k", data=body, headers=req.headers)
+        assert resp.status == 200
+        return
---
+++ transformer/github_webhooks.py
@@ -0,0 +1,39 @@
+import logging
+import hmac
+import json
+
+from aiohttp import web
+
+from .const import *
+
+
+logger = logging.getLogger(__name__)
+
+
+async def handle_github_webhook(req):
+    settings = req.app[SETTINGS]
+    session = req.app[HTTP_SESSION]
+    github_event = req.headers.get('X-GitHub-Event')
+    logger.info(req.headers)
+    try:
+        github_user = req.match_info['user']
+    except Exception:
+        raise web.HTTPBadRequest(reason='unknown event=%s/user=%s' % (github_event, req.match_info))
+    try:
+        logger.info('Received github webhook for event %s, user: %s', github_event, github_user)
+        hash_alg, body_signature = req.headers.get('X-Hub-Signature').split('=')
+        body = await req.read()
+        expected_signature = hmac.new(settings[GH_SECRET].encode(), msg=body, digestmod=hash_alg).hexdigest()
+        if not body_signature == expected_signature:
+            msg = 'Invalid {} signature, got {} but expected: {}'.format(hash_alg, body_signature, expected_signature)
+            logger.debug(msg)
+            raise web.HTTPForbidden(reason=msg)
+    except (KeyError,):
+        raise web.HTTPForbidden(reason='invalid body signature')
+    # accepted events
+    if github_event not in ['push']:
+        logger.debug('Unexpected event arrived, skipping..')
+        return web.HTTPOk()
+    payload = json.loads(body)
+
+    return web.HTTPOk()
