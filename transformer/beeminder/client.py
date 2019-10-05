a6f22fd73d758e609bac15a4080f1a07fd0ab2f8
Lukasz Jachym <lukasz.jachym@gmail.com>
2019-10-05 11:10:59 +0200

    fixes: http exception handling
    - forwards errors from json response
    - excludes empty params

--- transformer/beeminder/client.py
+++ transformer/beeminder/client.py
@@ -1,6 +1,10 @@
+import logging
 from urllib.parse import urljoin
 
 from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientError, ClientResponseError
+
+logger = logging.getLogger(__name__)
 
 
 class BeeminderClient:
@@ -15,11 +19,24 @@ class BeeminderClient:
     async def _make_request(self, *args, **kwargs):
         if self._session is None:
             self._session = self._session_factory()
-        query_params = kwargs['params'] or {}
+        query_params = kwargs.get('params', {})
+        query_params = {param: value for param, value in query_params.items() if value is not None}
         query_params.update(dict(auth_token=self._auth_token))
-        kwargs.update(dict(raise_for_status=True))
+        kwargs.update(dict(params=query_params))
+        logger.info('Query params: %s', kwargs['params'])
         async with self._session.request(*args, **kwargs) as resp:
-            return await resp.json()
+            payload = await resp.json()
+            try:
+                resp.raise_for_status()
+                return payload
+            except ClientResponseError as ex:
+                reason = payload.get('errors') or getattr(ex, 'reason')
+                updated_resp_error = ClientResponseError(ex.request_info,
+                                                         ex.history,
+                                                         status=ex.status,
+                                                         message=reason,
+                                                         headers=ex.headers)
+                raise updated_resp_error
 
     def _get_endpoint_url(self, url):
         return urljoin(self.BASE_URL, url)
@@ -44,7 +61,7 @@ class BeeminderClient:
             datapoints=datapoints
         )
         url = self._get_endpoint_url('users/{username}/goals/{goal_name}'.format(username=username,
-                                                                                  goal_name=goal_name))
+                                                                                 goal_name=goal_name))
         return await self._make_request('GET', url, params=params)
 
     async def get_all_goals(self, username: str = None):
@@ -61,7 +78,7 @@ class BeeminderClient:
         return await self._make_request('GET', url, params=params)
 
     async def create_datapoint(self, goal_name, value, username: str = None, timestamp: int = None,
-                               daystamp: str = '', comment: str = '', requestid: str = ''):
+                               daystamp: str = None, comment: str = None, requestid: str = None):
         username = username if username else self._username
         params = dict(
             value=value,
@@ -71,14 +88,14 @@ class BeeminderClient:
             requestid=requestid
         )
         url = self._get_endpoint_url('users/{user}/goals/{goal}/datapoints.json'.format(user=username,
-                                                                                         goal=goal_name))
+                                                                                        goal=goal_name))
         return await self._make_request('POST', url, params=params)
 
     async def update_datapoint(self, goal_name, datapoint_id, value, username: str = None, timestamp: int = None,
                                comment: str = None):
         username = username if username else self._username
         url = self._get_endpoint_url('users/{u}/goals/{g}/datapoints/{id}.json'.format(u=username, g=goal_name,
-                                                                                        id=datapoint_id))
+                                                                                       id=datapoint_id))
         params = dict(
             value=value,
             timestamp=timestamp,
@@ -89,5 +106,5 @@ class BeeminderClient:
     async def delete_datapoint(self, goal_name, datapoint_id, username: str = None):
         username = username if username else self._username
         url = self._get_endpoint_url('users/{u}/goals/{g}/datapoints/{id}.json'.format(u=username, g=goal_name,
-                                                                                        id=datapoint_id))
+                                                                                       id=datapoint_id))
         return await self._make_request('DELETE', url)
